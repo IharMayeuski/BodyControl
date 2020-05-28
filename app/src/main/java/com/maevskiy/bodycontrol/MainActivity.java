4d2d4d9cc05b129caf92582e1ff36d4b3782f808
Maevskiy <maevskij.i@gmail.com>
2020-05-28 23:37:40 +0300

    First excess

--- app/src/main/java/com/maevskiy/bodycontrol/MainActivity.java
+++ app/src/main/java/com/maevskiy/bodycontrol/MainActivity.java
@@ -6,71 +6,75 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    ArrayList<String> phones = new ArrayList();
-    ArrayAdapter<String> adapter;
-
-    ArrayList<String> selectedPhones = new ArrayList();
+    ArrayList<String> productForChoice = new ArrayList();
+    ArrayList<String> products = new ArrayList();
+    ArrayList<String> weights = new ArrayList();
+    ArrayAdapter<String> adapterProducts;
+    ArrayAdapter<String> adapterForChoice;
+    ArrayAdapter<String> adapterForWeights;
+    ArrayList<String> selectedProducts = new ArrayList();
     ListView productList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        productForChoice.add("-");
+        productForChoice.add("Bread");
+        productForChoice.add("Brockolly");
 
-        phones.add("iPhone 7");
-        phones.add("Samsung Galaxy S7");
-        phones.add("Google Pixel");
-        phones.add("Huawei P10");
-        phones.add("HP Elite z3");
+        weights.add("-");
+        weights.add("50");
+        weights.add("100");
+
+        Spinner spinner = (Spinner) findViewById(R.id.products);
+        adapterForChoice = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, productForChoice);
+        adapterForChoice.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapterForChoice);
+
+        Spinner spinnerChoiceWeight = (Spinner) findViewById(R.id.productweights);
+        adapterForWeights = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, weights);
+        adapterForWeights.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerChoiceWeight.setAdapter(adapterForWeights);
 
         productList = (ListView) findViewById(R.id.productList);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_multiple_choice, phones);
-        productList.setAdapter(adapter);
-
+        adapterProducts = new ArrayAdapter<>(this, android.R.layout.simple_list_item_multiple_choice, products);
+        productList.setAdapter(adapterProducts);
         // обработка установки и снятия отметки в списке
-        productList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+        productList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
-            {
-                // получаем нажатый элемент
-                String phone = adapter.getItem(position);
-                if(productList.isItemChecked(position)==true){
-                    selectedPhones.add(phone);
-                }
-                else{
-
-                    selectedPhones.remove(phone);
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                String product = adapterForChoice.getItem(position);
+                if (productList.isItemChecked(position)) {
+                    selectedProducts.add(product);
+                } else {
+                    selectedProducts.remove(product);
                 }
             }
         });
     }
 
-    public void add(View view){
-
-        EditText phoneEditText = (EditText) findViewById(R.id.phone);
-        String phone = phoneEditText.getText().toString();
-        if(!phone.isEmpty() && phones.contains(phone)==false){
-            adapter.add(phone);
-            phoneEditText.setText("");
-            adapter.notifyDataSetChanged();
+    public void add(View view) {
+        Spinner spinner = (Spinner) findViewById(R.id.products);
+        String product = spinner.getSelectedItem().toString();
+        if (!product.isEmpty() && !products.contains(product)) {
+            adapterProducts.add(product);
+            adapterProducts.notifyDataSetChanged();
         }
     }
-    public void remove(View view){
-        // получаем и удаляем выделенные элементы
-        for(int i=0; i< selectedPhones.size();i++){
-            adapter.remove(selectedPhones.get(i));
-        }
-        // снимаем все ранее установленные отметки
-        productList.clearChoices();
-        // очищаем массив выбраных объектов
-        selectedPhones.clear();
 
-        adapter.notifyDataSetChanged();
+    public void remove(View view) {
+        for (int i = 0; i < selectedProducts.size(); i++) {
+            adapterProducts.remove(selectedProducts.get(i));
+        }
+        productList.clearChoices();
+        selectedProducts.clear();
+        adapterProducts.notifyDataSetChanged();
     }
 }
